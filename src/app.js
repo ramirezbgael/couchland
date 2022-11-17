82734ae14622dea4f8e8a498c2a75d027b7822e8
Gael Ramirez <gael1423@gmail.com>
2022-11-16 18:01:44 -0600

    correction

--- src/app.js
+++ src/app.js
@@ -2,14 +2,11 @@ const cors = require('cors')
 const express = require('express')
 const authRouter = require('./auth/auth.router')
 const usersRouter = require('./users/users.router')
-const categoriesRouter = require('./categories/categories.router')
-const recipesRouter = require('./recipes/recipes.router')
-const ingredientsRouter = require('./ingredients/ingredients.router')
+
 const {port} = require('./config')
 const app = express()
 
 app.use(express.json())
-app.use(cors())
 
 app.get('/', (req, res) => {
   res.status(200).json({
@@ -29,9 +26,6 @@ db.sync()
 
 app.use('/api/v1/users', usersRouter)
 app.use('/api/v1/auth', authRouter)
-app.use('/api/v1/categories', categoriesRouter)
-app.use('/api/v1/recipes', recipesRouter)
-app.use('/api/v1/ingredients', ingredientsRouter)
 
 app.listen(port, ()=>{
   console.log(`Server started at port ${port}`)
